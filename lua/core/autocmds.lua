51e593b44dd4600a70c624c50191d43c2c458c06
Kirxii <131458787+Kirxii@users.noreply.github.com>
2025-12-02 06:19:44 +0700

    FIX: Stops producing an error when running the setup on something other than Wezterm

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -5,84 +5,86 @@
 -- Neovim and Wezterm colorschemes syncing
 -- Saved Neovim colorschemes between sessions
 vim.api.nvim_create_autocmd("ColorScheme", {
-  group = vim.api.nvim_create_augroup("wezterm_colorscheme", { clear = true }),
-  callback = function(args)
-    local colorschemes = {
-      ["default"] = "",
-      ["catppuccin-frappe"] = "Catppuccin Frappe",
-      ["catppuccin-latte"] = "Catppuccin Latte",
-      ["catppuccin-macchiato"] = "Catppuccin Macchiato",
-      ["catppuccin-mocha"] = "Catppuccin Mocha",
-      ["dayfox"] = "dayfox",
-      ["dawnfox"] = "dawnfox",
-      ["carbonfox"] = "carbonfox",
-      ["nordfox"] = "nordfox",
-      ["nightfox"] = "nightfox",
-      ["duskfox"] = "duskfox",
-      ["terafox"] = "terafox",
-      ["monochrome"] = "Mono (terminal.sexy)",
-      ["rose-pine-main"] = "rose-pine",
-      ["rose-pine-moon"] = "rose-pine-moon",
-      ["rose-pine-dawn"] = "rose-pine-dawn",
-      ["github_light"] = "Github",
-      ["github_dark"] = "Github Dark",
-      ["dracula"] = "Dracula (Official)",
-      ["dracula-soft"] = "Dracula (Official)",
-      ["oxocarbon"] = "Oxocarbon Dark (Gogh)",
-      ["onedark"] = "One Dark (Gogh)",
-      ["everblush"] = "Everblush",
-    }
+	group = vim.api.nvim_create_augroup("wezterm_colorscheme", { clear = true }),
+	callback = function(args)
+		local colorschemes = {
+			["default"] = "",
+			["catppuccin-frappe"] = "Catppuccin Frappe",
+			["catppuccin-latte"] = "Catppuccin Latte",
+			["catppuccin-macchiato"] = "Catppuccin Macchiato",
+			["catppuccin-mocha"] = "Catppuccin Mocha",
+			["dayfox"] = "dayfox",
+			["dawnfox"] = "dawnfox",
+			["carbonfox"] = "carbonfox",
+			["nordfox"] = "nordfox",
+			["nightfox"] = "nightfox",
+			["duskfox"] = "duskfox",
+			["terafox"] = "terafox",
+			["monochrome"] = "Mono (terminal.sexy)",
+			["rose-pine-main"] = "rose-pine",
+			["rose-pine-moon"] = "rose-pine-moon",
+			["rose-pine-dawn"] = "rose-pine-dawn",
+			["github_light"] = "Github",
+			["github_dark"] = "Github Dark",
+			["dracula"] = "Dracula (Official)",
+			["dracula-soft"] = "Dracula (Official)",
+			["oxocarbon"] = "Oxocarbon Dark (Gogh)",
+			["onedark"] = "One Dark (Gogh)",
+			["everblush"] = "Everblush",
+		}
 
-    local colorscheme = colorschemes[args.match]
-    if not colorscheme then
-      return
-    end
+		local colorscheme = colorschemes[args.match]
+		if not colorscheme then
+			return
+		end
 
-    local neovim_dir = vim.fn.stdpath("config") .. "/lua/core/colorscheme"
-    local neovim_file = require("KUtilities").read_file(neovim_dir)
-    local neovim_colorscheme = string.gsub(neovim_file, "[%l%p]+$", args.match)
-    neovim_colorscheme = string.gsub(neovim_colorscheme, "^\n+", "")
+		local neovim_dir = vim.fn.stdpath("config") .. "/lua/core/colorscheme"
+		local neovim_file = require("KUtilities").read_file(neovim_dir)
+		local neovim_colorscheme = string.gsub(neovim_file, "[%l%p]+$", args.match)
+		neovim_colorscheme = string.gsub(neovim_colorscheme, "^\n+", "")
 
-    local neovim_write = io.open(neovim_dir, "w")
-    if neovim_write then
-      neovim_write:write(neovim_colorscheme)
-      neovim_write:close()
-    end
+		local neovim_write = io.open(neovim_dir, "w")
+		if neovim_write then
+			neovim_write:write(neovim_colorscheme)
+			neovim_write:close()
+		end
 
-    local wezterm_dir = vim.fn.expand("~/.wezterm.lua")
-    local wezterm_read = io.open(wezterm_dir, "r")
-    local config
-    if wezterm_read then
-      config = string.gsub(
-        wezterm_read:read("*a"),
-        'config%.color_scheme = ".-"',
-        'config%.color_scheme = "' .. colorscheme .. '"'
-      )
-      wezterm_read:close()
-    end
+		local wezterm_dir = vim.fn.expand("~/.wezterm.lua")
+		local wezterm_read = io.open(wezterm_dir, "r")
+		local config
+		if wezterm_read then
+			config = string.gsub(
+				wezterm_read:read("*a"),
+				'config%.color_scheme = ".-"',
+				'config%.color_scheme = "' .. colorscheme .. '"'
+			)
+			wezterm_read:close()
+		end
 
-    local wezterm_write = io.open(wezterm_dir, "w")
-    if wezterm_write then
-      wezterm_write:write(config)
-      wezterm_write:close()
-    end
-  end,
+		local wezterm_write = io.open(wezterm_dir, "w")
+		if wezterm_write then
+			wezterm_write:write(config)
+			wezterm_write:close()
+		end
+	end,
 })
 
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
-  callback = function(event)
-    local title = "NVIM"
-    if event.file ~= "" then
-      title = string.format("NVIM: %s", vim.fs.basename(event.file))
-    end
+	callback = function(event)
+		if vim.fn.executable("wezterm") == 1 then
+			local title = "NVIM"
+			if event.file ~= "" then
+				title = string.format("NVIM: %s", vim.fs.basename(event.file))
+			end
 
-    vim.fn.system { "wezterm", "cli", "set-tab-title", title }
-  end,
+			vim.fn.system({ "wezterm", "cli", "set-tab-title", title })
+		end
+	end,
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "md" },
-  callback = function()
-    require("ufo").detach()
-  end,
+	pattern = { "md" },
+	callback = function()
+		require("ufo").detach()
+	end,
 })
